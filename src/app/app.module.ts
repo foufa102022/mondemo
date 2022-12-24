ef7c104160ca8c8d6f995e7f06c57ca0ba5a9b57
foufa102022 <chetouiiftikhar@gmail.com>
2022-12-24 10:21:18 +0100

    first c

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppareilsComponent } from './appareils/appareils.component';
+import { AppareilItemComponent } from './appareil-item/appareil-item.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppareilsComponent,
+    AppareilItemComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
